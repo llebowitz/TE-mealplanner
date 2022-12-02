7aa8ace00666980188e73260921a02df1a6c8b1c
Steven Arenaz <arenazs@gmail.com>
2022-12-02 15:46:02 -0500

    Database shell script updated. Run create.sh to update your local DB with tables from brainstorming session

--- java/database/data.sql
+++ java/database/data.sql
@@ -2,5 +2,6 @@ BEGIN TRANSACTION;
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+INSERT INTO users (username,password_hash,role) VALUES ('bigFoody', '$2a$10$TdHW3cUNn0FMa3U2kR22Vu28C72TPjDE/9pOaYdMehgANPNeaFWLa', 'ROLE_USER');
 
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -10,4 +10,66 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+CREATE TABLE ingredients (
+    ingredient_id SERIAL,
+    ingredient_name varchar(100) NOT NULL,
+    CONSTRAINT PK_ingredient PRIMARY KEY (ingredient_id)
+);
+
+CREATE TABLE recipes(
+    recipe_id SERIAL,
+    recipe_name varchar(100) NOT NULL,
+    cook_time INT,
+    CONSTRAINT PK_recipe PRIMARY KEY (recipe_id)
+);
+
+CREATE TABLE tags(
+    tag_id SERIAL,
+    tag_name varchar(100) NOT NULL,
+    CONSTRAINT PK_tag PRIMARY KEY (tag_id)
+);
+
+CREATE TABLE meal_plan(
+    plan_id SERIAL,
+    CONSTRAINT PK_plan PRIMARY KEY (plan_id)
+);
+
+CREATE TABLE user_meal_plan(
+    user_id INT NOT NULL,
+    plan_id INT NOT NULL,
+    CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(user_id),
+    CONSTRAINT FK_plan_id FOREIGN KEY (plan_id) REFERENCES meal_plan(plan_id)
+);
+
+CREATE TABLE recipes_tags(
+    recipe_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    CONSTRAINT FK_recipe_id FOREIGN KEY (recipe_id) REFERENCES recipes(recipe_id),
+    CONSTRAINT FK_tag_id FOREIGN KEY (tag_id) REFERENCES tags(tag_id)
+);
+
+CREATE TABLE recipes_ingredients(
+    recipe_id INT NOT NULL,
+    ingredient_id INT NOT NULL,
+    amount INT,
+    measurement varchar(50),
+    CONSTRAINT FK_recipe_id FOREIGN KEY (recipe_id) REFERENCES recipes(recipe_id),
+    CONSTRAINT FK_ingredient_id FOREIGN KEY (ingredient_id) REFERENCES ingredients(ingredient_id)
+);
+
+CREATE TABLE users_recipes(
+    recipe_id INT NOT NULL,
+    user_id INT NOT NULL,
+    CONSTRAINT FK_recipe_id FOREIGN KEY (recipe_id) REFERENCES recipes(recipe_id),
+    CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE recipes_meal_plan(
+    recipe_id INT NOT NULL,
+    plan_id INT NOT NULL,
+    CONSTRAINT FK_recipe_id FOREIGN KEY (recipe_id) REFERENCES recipes(recipe_id),
+    CONSTRAINT FK_plan_id FOREIGN KEY (plan_id) REFERENCES meal_plan(plan_id)
+);
+
+
 COMMIT TRANSACTION;
