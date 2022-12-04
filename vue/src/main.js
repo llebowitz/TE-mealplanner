b1c72c9cc645d1ccd97db83cf26fb9ad4469debb
Zachary Luchette <luchettezac@gmail.com>
2022-12-04 16:59:06 -0500

    Added Font Awesome via the method in the Using Front Awesome pdf in the capstone Drive folder

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,17 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router/index'
-import store from './store/index'
-import axios from 'axios'
-import vuetify from './plugins/vuetify'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router/index';
+import store from './store/index';
+import axios from 'axios';
+import vuetify from './plugins/vuetify';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	vuetify,
+	render: (h) => h(App),
+}).$mount('#app');
