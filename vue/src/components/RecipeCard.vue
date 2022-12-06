6f16860cecbd12389725cc9f0c6983018c14617c
Liora Lebowitz <lioralebowitz@gmail.com>
2022-12-06 14:59:46 -0500

    12/6 2:59pm

--- vue/src/components/RecipeCard.vue
+++ vue/src/components/RecipeCard.vue
@@ -1,29 +1,14 @@
 <template>
   <div>
-      <h3>{{ recipe.name}} </h3>
+      <h3>{{ recipe.recipeName}} </h3>
       <!-- add image -->
         <body> 
-            <!-- how do we pull this from database/external API? -->
             <div>{{ recipe.instructions }}</div>
             <div>{{ recipe.ingredients }}</div>
         </body>
         <!-- add event: on click, save to user recipes -->
         <button>Save Recipe</button>
         <!-- add event: on click, save to user meal plan
-            change button to dropdown with days of week
-        <form>Add to Meal Plan
-            <select>
-                <option>Monday</option>
-                <option>Tuesday</option>
-                <option>Wednesday</option>
-                <option>Thursday</option>
-                <option>Friday</option>
-                <option>Saturday</option>
-                <option>Sunday</option>
-            </select>
-        </form>
-
-        Add button to add recipe ingredients to grocery list
 
 </div>
 </template> -->
@@ -47,18 +32,17 @@ export default {
 
 }
 
-
- 
-
 <!-- methods: {
 saveRecipe()
 Should we set it up to automatically save a copy of the recipe here?
 
 unSaveRecipe()
 
-ddToMealPlan()
+addToMealPlan()
             'if Monday, add to ""'
 deleteFromMealPlan()
+
+toggleSave() 
 } -->
 
 </script>
