ca4d728c8cd10145b56d9b702f81eb9039a87bd5
Liora Lebowitz <lioralebowitz@gmail.com>
2022-12-06 10:41:53 -0500

    12/6 10:41am

--- vue/src/components/SavedRecipes.vue
+++ vue/src/components/SavedRecipes.vue
@@ -8,7 +8,7 @@ export default {
     methods:
 
     // modifyRecipe() {
-    //     Needs to clone the recipe and add to Saved Recipes
+    //     Needs to clone the recipe and add to Saved Recipes. Send username to database. Saved recipes will be passed back in an array.
     // }
 
 }
