b981606aaedfe70d583eb3a1b735414882577e76
Zachary Luchette <luchettezac@gmail.com>
2022-12-07 12:14:16 -0500

    Issue with RecipesDisplay

---
+++ vue/src/views/RecipesDisplay.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<recipe-search></recipe-search>
+		<add-recipe></add-recipe>
+	</div>
+</template>
+
+<script>
+import RecipeSearch from '../components/RecipeSearch.vue';
+import AddRecipe from '../components/AddRecipe.vue'; //Take out later; just for demo
+
+export default {
+	components: {
+		RecipeSearch,
+		AddRecipe, //Take out later, just for demo
+	},
+};
+</script>
+
+<style></style>
