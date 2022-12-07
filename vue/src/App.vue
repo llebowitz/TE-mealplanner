6a8298c1aa85ad342bb52f015e498dbe6a92ae2d
Liora Lebowitz <lioralebowitz@gmail.com>
2022-12-07 15:24:48 -0500

    12/7 3:24pm

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -5,7 +5,6 @@
 				<router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
 				<router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>&nbsp;|&nbsp;
 				<router-link v-bind:to="{ name: 'recipes' }">Go to Recipes Display</router-link>&nbsp;|&nbsp;
-				<router-link v-bind:to="{ name: 'add-recipe' }">Add a Recipe</router-link>
 			</div>
 
 			<v-main>
