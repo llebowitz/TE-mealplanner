b302118b2f8e6748c359f35b63c39eded124a1a5
Zachary Luchette <luchettezac@gmail.com>
2022-12-09 11:38:01 -0500

    Fixed up new recipe component a bit. Still have to add multiple ingredients functionality

--- vue/src/components/AddRecipe.vue
+++ vue/src/components/AddRecipe.vue
@@ -1,173 +1,106 @@
 <template>
-  <div id="app">
-  <v-app id="inspire">
-    <v-row justify="center">
-      <v-dialog class="recipe-form"
-        v-model="dialog"
-        fullscreen
-        hide-overlay
-        transition="dialog-bottom-transition"
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            color="primary"
-            dark
-            v-bind="attrs"
-            v-on="on"
-          >
-            Add Recipe
-          </v-btn>
-        </template>
-        <v-card>
-          <v-toolbar
-            dark
-            color="primary"
-          >
-            <v-btn
-              icon
-              dark
-              @click="dialog = false"
-            >
-              <v-icon>mdi-close</v-icon>
-            </v-btn>
+	<div id="app">
+		<v-app id="inspire">
+			<v-row justify="center">
+				<v-dialog class="recipe-form" v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+					<template v-slot:activator="{ on, attrs }">
+						<v-btn color="primary" dark v-bind="attrs" v-on="on"> Add Recipe </v-btn>
+					</template>
+					<v-card>
+						<v-toolbar dark color="primary">
+							<v-btn icon dark @click="dialog = false">
+								<v-icon>mdi-close</v-icon>
+							</v-btn>
 
-            <v-toolbar-title>New Recipe</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-toolbar-items>
-              <!-- Save button -->
-              <v-btn 
-                dark
-                text
-                v-on:click="addRecipe"
-              >
-                Save
-              </v-btn>
-            </v-toolbar-items>
-          </v-toolbar>
-          <v-list
-            three-line
-            subheader
-          >
-            <v-subheader>This will be added to your saved recipes.</v-subheader>
-          
-       
-        <v-card-text>
-          <v-container>
-            <v-row>
-            <v-col
-                cols="12"
-                sm="6"
-              >
-                <v-text-field v-model="newRecipe.recipeName"
-                  label="Recipe Name*"
-                  required
-                ></v-text-field>
-     
-              </v-col>
-             <v-col cols="3">
-          <v-autocomplete
-                  :items="[]"
-                  label="Ingredient"
-                  multiple
-                ></v-autocomplete>
-        </v-col>
-           <v-col cols="3">
-          <v-text-field
-            label="Amount"
-            placeholder="1"
-          ></v-text-field>
-        </v-col>
+							<v-toolbar-title>New Recipe</v-toolbar-title>
+							<v-spacer></v-spacer>
+							<v-toolbar-items>
+								<!-- Save button -->
+								<v-btn dark text v-on:click="addRecipe"> Save </v-btn>
+							</v-toolbar-items>
+						</v-toolbar>
+						<v-list three-line subheader>
+							<v-subheader>This will be added to your saved recipes.</v-subheader>
 
-        <v-col cols="6">
-          <v-select
-          
-            :items="items"
-            item-text="state"
-            item-value="abbr"
-            label="Unit of Measurement"
-            persistent-hint
-            return-object
-            single-line
-          ></v-select>
-           <v-btn
-      class="mx-2"
-      fab
-      dark
-      color="indigo"
-    >
-      <v-icon dark>
-        mdi-plus
-      </v-icon>
-    </v-btn>
-        </v-col>
+							<v-card-text>
+								<v-container>
+									<v-row>
+										<v-col cols="12" sm="6">
+											<v-text-field v-model="newRecipe.recipeName" label="Recipe Name*" required></v-text-field>
+										</v-col>
+									</v-row>
+									<v-row>
+										<v-col cols="3">
+											<v-autocomplete :items="[]" label="Ingredient" multiple></v-autocomplete>
+										</v-col>
 
-             <v-textarea v-model="newRecipe.instructions"
-      name="input-7-1"
-      filled
-      label="Cooking Instructions"
-      auto-grow
-      value=""
-    ></v-textarea>
-            </v-row>
-          </v-container>
-          <small>* indicates required field</small>
-        </v-card-text>
-        <v-card-actions />
-          <v-spacer></v-spacer>
-        
-          </v-list>
-        </v-card>
-      </v-dialog>
-    </v-row>
-  </v-app>
+										<v-col cols="3">
+											<v-text-field label="Amount" placeholder="1"></v-text-field>
+										</v-col>
 
-</div>
+										<v-col cols="2">
+											<v-select :items="items" item-text="state" item-value="abbr" label="Unit of Measurement" persistent-hint return-object single-line></v-select>
+										</v-col>
+										<v-col cols="4">
+											<v-btn class="mx-2" fab dark color="indigo">
+												<v-icon dark> mdi-hamburger-plus </v-icon>
+											</v-btn>
+										</v-col>
+									</v-row>
+
+									<v-row>
+										<v-col>
+											<v-textarea v-model="newRecipe.instructions" name="input-7-1" filled label="Cooking Instructions" auto-grow value=""></v-textarea>
+										</v-col>
+									</v-row>
+								</v-container>
+								<small class="text-right">* indicates required field</small>
+							</v-card-text>
+							<v-card-actions />
+							<!-- <v-spacer></v-spacer> -->
+						</v-list>
+					</v-card>
+				</v-dialog>
+			</v-row>
+		</v-app>
+	</div>
 </template>
 
 <script>
-import AppService from "../services/AppService";
+import AppService from '../services/AppService';
 
 export default {
-data () {
-    return {
-      dialog: false,
-      notifications: false,
-      sound: true,
-      widgets: false,
-      items: [
-        { state: 'g' },
-        { state: 'oz' },
-        { state: 'item' },
-        { state: 'lb' },
-        { state: 'cups'},
-        { state: 'tablespoons'},
-        { state: 'teaspoons'},
-        ],
-      newRecipe: {
-        recipeName: '',
-        ingredient: '',
-        amount: '',
-      }
-    }
-  },
+	data() {
+		return {
+			dialog: false,
+			notifications: false,
+			sound: true,
+			widgets: false,
+			items: [{ state: 'g' }, { state: 'oz' }, { state: 'item' }, { state: 'lb' }, { state: 'cups' }, { state: 'tablespoons' }, { state: 'teaspoons' }],
+			newRecipe: {
+				recipeName: '',
+				ingredient: '',
+				amount: '',
+			},
+		};
+	},
 	methods: {
-    addRecipe() {
-      AppService.addRecipe(this.newRecipe).then( (response) => {
-        if (response.status === 201) {
-          this.resetForm();
-          this.dialog = false;
-        }
-      })
-    },
+		addRecipe() {
+			AppService.addRecipe(this.newRecipe).then((response) => {
+				if (response.status === 201) {
+					this.resetForm();
+					this.dialog = false;
+				}
+			});
+		},
 
-    resetForm() {
-      this.newRecipe = {
-        recipeName: '',
-        ingredient: '',
-        amount: '',
-      };
-    },
-
-  },
+		resetForm() {
+			this.newRecipe = {
+				recipeName: '',
+				ingredient: '',
+				amount: '',
+			};
+		},
+	},
 };
 </script>
