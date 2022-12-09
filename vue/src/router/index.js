55e63ebe45b20fe13c19d7fbcf6ca59181762bba
Zachary Luchette <luchettezac@gmail.com>
2022-12-09 12:09:59 -0500

    Added route to recipe in RecipeCard. About to start creating Recipe view layout etc.

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -6,6 +6,7 @@ import Logout from '../views/Logout.vue';
 import Register from '../views/Register.vue';
 import store from '../store/index';
 import RecipesDisplay from '../views/RecipesDisplay.vue';
+import Recipe from '../views/Recipe.vue';
 
 Vue.use(Router);
 
@@ -58,6 +59,14 @@ const router = new Router({
 			path: '/recipes',
 			name: 'recipes',
 			component: RecipesDisplay,
+			meta: {
+				requiresAuth: false,
+			},
+		},
+		{
+			path: '/singleRecipe',
+			name: 'singleRecipe',
+			component: Recipe,
 			meta: {
 				requiresAuth: false,
 			},
