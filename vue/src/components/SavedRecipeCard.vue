946dc629e54725ed360f2e51f10d5da0c693b5ec
Katy Powers <katy.e.powers@gmail.com>
2022-12-11 17:47:26 -0500

    saved recipes

--- vue/src/components/SavedRecipeCard.vue
+++ vue/src/components/SavedRecipeCard.vue
@@ -2,8 +2,9 @@
   <div>
         
           <div class="saved-recipe-card">
-          <img height="300" max-width="300" v-bind:src="`${recipe.imgLink}`">
+          
           <div class="saved-container">
+              <img v-bind:src="`${recipe.imgLink}`" >
               <router-link v-bind:to="{ name : 'singleRecipe', params: { recipeId: recipe.id } }">
               <h3>{{recipe.name}}</h3> </router-link>
               <p>{{recipe.yield}}</p>
@@ -22,18 +23,25 @@ export default {
 
 <style>
 .saved-recipe-card {
-    border: 1px black solid;
+    border: 2px black solid;
     width: 25%;
-    height: 25%
-
+    height: auto;
+    border-radius: 25px;
+    margin: 5px;
+    padding: 8px
+    
 }
+ .saved-recipe-card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+
 .saved-container {
 
     height: auto;
-    width: 100%%;
-    border: 1px black solid
+    width: 100%;
+ 
     
-   
 }
 h3 {
     font-size: 14px
@@ -41,6 +49,11 @@ h3 {
 
 p{
     font-size: 12px;
+}
+
+img {
+    width: 100%;
+    width: 100%
 }
 
 
