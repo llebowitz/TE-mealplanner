45c4d8de16a38ac854e80adda4e30d9ab52cf4d7
Steven Arenaz <arenazs@gmail.com>
2022-12-11 16:35:13 -0500

    Fixed recipecard sizing for recipe search

--- vue/src/components/RecipeCard.vue
+++ vue/src/components/RecipeCard.vue
@@ -1,7 +1,7 @@
 <template>
-	<v-card class="ma-1 pa-1 rounded-xl" max-width="550" outlined elevation="3">
+	<v-card class="ma-1 pa-1 rounded-xl" max-width="425" outlined elevation="3">
 		<router-link v-bind:to="{ name: 'singleRecipe', params: { recipeId: recipe.id } }">
-			<v-img height="450" max-width="450" v-bind:src="`${recipe.imgLink}`" lazy-src="https://i.pinimg.com/originals/f9/98/0f/f9980fdb73ff0acc69d70a8997acb5fa.gif">
+			<v-img height="300" max-width="300" v-bind:src="`${recipe.imgLink}`" lazy-src="https://i.pinimg.com/originals/f9/98/0f/f9980fdb73ff0acc69d70a8997acb5fa.gif">
 				<template v-slot:placeholder>
 					<v-row class="fill-height ma-0" align="center" justify="center"><v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular> </v-row></template></v-img
 		></router-link>
