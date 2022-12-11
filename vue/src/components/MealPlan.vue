53905ffb54c954b0be39c061004aaced7c3d34c8
Katy Powers <katy.e.powers@gmail.com>
2022-12-11 09:29:01 -0500

    mealplan

--- vue/src/components/MealPlan.vue
+++ vue/src/components/MealPlan.vue
@@ -2,10 +2,28 @@
   <div>
       <h2>Sunday</h2>
       <ul>
-        <!-- <li v-on="getMealPlanRecipes" v-for="recipe in recipes" v-bind:key="">{{recipe.name}}</li> -->
-
-
+        <li v-for="recipe in mealPlanRecipes.recipes" v-bind:key="recipe.id">{{recipe.name}}</li>
       </ul>
+
+     <tr>
+          <th>Sun</th>
+          <th>Mon</th>
+          <th>Tue</th>
+          <th>Wed</th>
+          <th>Thi</th>
+          <th>Fri</th>
+          <th>Sat</th>
+          
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
 
 
 
@@ -21,6 +39,12 @@ import AppService from '../services/AppService'
 export default {
 
     props: ['recipe', 'mealplan'],
+    data() {
+      return {
+        mealPlanRecipes: [],
+
+      };
+    },
 
     methods: {
       getMealPlanRecipes(){
@@ -28,9 +52,14 @@ export default {
           if(response.status === 200){
             alert("Here are your recipes");
           }
+          this.mealPlanRecipes = response.data;
         })
       }
 
+      },
+
+      created() {
+        this.getMealPlanRecipes();
       }
 
       // updateRecipeDay()
