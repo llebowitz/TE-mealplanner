59fbe3bfbbc656b004b2c6bb64563dd1637381f3
Liora Lebowitz <lioralebowitz@gmail.com>
2022-12-11 23:16:21 -0500

    12/11 11:16pm

--- vue/src/components/MealPlan.vue
+++ vue/src/components/MealPlan.vue
@@ -5,60 +5,65 @@
         <li v-for="(recipe, index) in mealPlanRecipes.sunday" v-bind:key="index">{{recipe.name}}</li>
       </ul> -->
       <h1>My Meal Plan</h1>
-    <div class="day-of-week">
+    <div class="week">
      
-      <div>Sun</div>
-      <div>Mon</div>
-      <div>Tues</div>
-      <div>Wed</div>
-      <div>Thurs</div>
-      <div>Fri</div>
-      <div>Sat</div>
+     <div id="sunday">
+      <div class="day">Sun</div>
+        <ul class="recipe">
+              <li v-for="(recipe, index) in mealPlanRecipes.sunday" v-bind:key="index">{{recipe.name}}</li>
+          </ul>
+      </div>
 
-    <div class="day-grid">
-     <ul>
-       <li v-for="(recipe, index) in mealPlanRecipes.sunday" v-bind:key="index">{{recipe.name}}</li>
-     </ul>
-    
-         </div>
-     <ul>
-       <li v-for="(recipe, index) in mealPlanRecipes.monday" v-bind:key="index">{{recipe.name}}</li>
-     </ul>
+      <div id="monday">
+        <div class="day">Mon</div>
+        <ul class="recipe">
+              <li v-for="(recipe, index) in mealPlanRecipes.monday" v-bind:key="index">{{recipe.name}}</li>
+          </ul>
+      </div>
 
-     <ul>
-       <li v-for="(recipe, index) in mealPlanRecipes.tuesday" v-bind:key="index">{{recipe.name}}</li>
-     </ul>
+      <div id="tuesday">
+        <div class="day">Tues</div>
+        <ul class="recipe">
+          <li v-for="(recipe, index) in mealPlanRecipes.tuesday" v-bind:key="index">{{recipe.name}}</li>
+        </ul>
+      </div>
 
-       <ul>
-       <li v-for="(recipe, index) in mealPlanRecipes.wednesday" v-bind:key="index">{{recipe.name}}</li>
-     </ul>
+      <div id="wednesday">
+        <div class="day">Wed</div>
+        <ul class="recipe">
+          <li v-for="(recipe, index) in mealPlanRecipes.wednesday" v-bind:key="index">{{recipe.name}}</li>
+        </ul>
+      </div>
 
-     <ul>
-       <li v-for="(recipe, index) in mealPlanRecipes.thursday" v-bind:key="index">{{recipe.name}}</li>
-     </ul>
+      <div id="thursday">
+        <div class="day">Thurs</div>
+        <ul class="recipe">
+          <li v-for="(recipe, index) in mealPlanRecipes.thursday" v-bind:key="index">{{recipe.name}}</li>
+        </ul>
+      </div>
 
-      <ul>
-       <li v-for="(recipe, index) in mealPlanRecipes.friday" v-bind:key="index">{{recipe.name}}</li>
-     </ul>
+      <div id="friday">
+        <div class="day">Fri</div>
+        <ul class="recipe">
+          <li v-for="(recipe, index) in mealPlanRecipes.friday" v-bind:key="index">{{recipe.name}}</li>
+        </ul>
+      </div>
 
-      <ul>
-       <li v-for="(recipe, index) in mealPlanRecipes.saturday" v-bind:key="index">{{recipe.name}}</li>
-     </ul>
-   
+      <div id="saturday">
+        <div class="day">Sat</div>
+        <ul class="recipe">
+          <li v-for="(recipe, index) in mealPlanRecipes.saturday" v-bind:key="index">{{recipe.name}}</li>
+        </ul>
+      </div>
 
-    
-
-   
-
+     </div>
    
 
 </div>
 
 
-
-
       <!--Button to add ingredients to Grocery List -->
-  </div>
+
 </template>
 
 <script>
@@ -102,13 +107,59 @@ export default {
 </script>
 
 <style>
-.day-of-week,
-.day-grid{
+.week{
   display: grid;
   grid-template-columns: repeat(7, 1fr);
+  grid-template-areas: "sunday monday tuesday wednesday thursday friday saturday";
+  width: 100%;
+  border: solid black;
+  border-left: hidden;
 }
 
-.day-grid{
-  font-size: 12px
+#sunday{
+  grid-area: sunday;
 }
+
+#monday{
+  grid-area: monday;
+}
+
+#tuesday{
+  grid-area: tuesday;
+}
+
+#wednesday{
+  grid-area: wednesday;
+}
+
+#thursday{
+  grid-area: thursday;
+}
+
+#friday{
+  grid-area: friday;
+}
+
+#saturday{
+  grid-area: saturday;
+}
+
+.day{
+  font-weight: bold;
+  font-size: 30px;
+  border-left: solid black;
+  padding-left: 5px;
+}
+
+.recipe{
+  color: blue;
+  border-left: solid black;
+  border-top: solid black;
+  padding-left: 5px;
+}
+
+ul {
+  list-style-image: url(../assets/carrot.png);
+}
+
 </style>
